3d3c4b960441dd2465232a526bc8ea20aba798f8
Axel Svensson <mail@axelsvensson.com>
2025-11-13 15:30:59 +0100

    Add ISO and US paper sizes to pdf-create-empty
    - Add ISO paper sizes A0 - C10
    - Add US paper sizes letter, legal, ledger and tabloid

--- tests/test_empty.py
+++ tests/test_empty.py
@@ -12,6 +12,8 @@ def test_pdf_create_empty():
             ["Invalid dimensions", "-d10PTx100pt"],
             ["Invalid dimensions", "-d2.5.5cmx1in"],
             ["Invalid dimensions", "-d5cmxcm"],
+            ["Invalid dimensions", "-d2A0"],
+            ["Invalid dimensions", "-dLeggal"],
     ]:
         info(f"========== test_empty Begin case invalid: {pos_args}")
         empty_pdf = intmp('pdf', "Should not exist")
@@ -29,6 +31,12 @@ def test_pdf_create_empty():
             [100, 36, "empty-100ptx36pts.pdf", "-d100ptx36pts"],
             [0.6789*_in, 2.3456*_cm, "empty-.6789inx2.3456cm.pdf", "-d.6789inx2.3456cm"],
             [3*_cm, 20*_mm, "empty-3cmx20mm.pdf", "-d3cmx20mm"],
+            [841*_mm, 1189*_mm, "empty-A0.pdf", "-dA0"],
+            [297*_mm, 210*_mm, "empty-a4L.pdf", "-da4L"],
+            [31*_mm, 44*_mm, "empty-B10.pdf", "-dB10"],
+            [229*_mm, 162*_mm, "empty-C5l.pdf", "-dC5l"],
+            [8.5*_in, 14*_in, "empty-LeGaL.pdf", "-dLeGaL"],
+            [11*_in, 8.5*_in, "empty-LettErL.pdf", "-dLettErL"],
     ]:
         info(f"========== test_empty Begin case {default_fname} = {pos_args}")
         wpt = round(wpt)
